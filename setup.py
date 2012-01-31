4f10895b1386af76497af28d0036436130ca952b
Doug Winter <doug.winter@isotoma.com>
2012-01-31 16:19:13 +0000

    moving to zest.releaser

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.0.0'
+version = '1.0.1dev'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
