acf513c19d9e777068019ceb394a937a4e7973ca
Doug Winter <doug.winter@isotoma.com>
2012-01-31 16:33:58 +0000

    Preparing release 1.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.0.2dev'
+version = '1.0.2'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
