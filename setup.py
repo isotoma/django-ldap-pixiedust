05e762ab21a74ce588c531a40d3e191277f7f78e
Doug Winter <doug.winter@isotoma.com>
2012-01-31 16:34:04 +0000

    Back to development: 1.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.0.2'
+version = '1.0.3dev'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
